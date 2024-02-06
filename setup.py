8c7a4c5d7636704e03ceb25096f7f5710a10adc9
Minh-Tri Pham <pmtri80@gmail.com>
2024-02-06 05:49:57 +0000

    initial version

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_namespace_packages
+from mt.gl.version import version
+
+setup(
+    name="mtopengl",
+    version=version,
+    description="Minh-Tri Pham's extra modules for dealing with OpenGL in Python",
+    author=["Minh-Tri Pham"],
+    packages=find_namespace_packages(include=["mt.*"]),
+    # scripts=[
+    #     "scripts/immview",
+    # ],
+    install_requires=[
+        "glm",
+        "pyopengl",
+        "glfw",
+        "mtbase",
+    ],
+)
