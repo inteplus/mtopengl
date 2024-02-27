31c0727fb60038006c51555f3ed84a8c9d07709b
Minh-Tri <minhtri.pham@winnowsolutions.com>
2024-02-27 15:27:55 +0000

    v0.1: using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_namespace_packages
-from mt.opengl.version import version
+
+VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION.txt")
 
 setup(
     name="mtopengl",
-    version=version,
     description="Minh-Tri Pham's extra modules for dealing with OpenGL in Python",
     author=["Minh-Tri Pham"],
     packages=find_namespace_packages(include=["mt.*"]),
@@ -13,9 +14,18 @@ setup(
     #     "scripts/immview",
     # ],
     install_requires=[
-        "pyglm",
         "pyopengl",
         "glfw",
+        "mtglm",
         # "mtbase>=4.25",  # we don't need mtbase yet
     ],
+    setup_requires=["setuptools-git-versioning<2"],
+    setuptools_git_versioning={
+        "enabled": True,
+        "version_file": VERSION_FILE,
+        "count_commits_from_version_file": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}+{branch}",
+        "dirty_template": "{tag}.post{ccount}",
+    },
 )
